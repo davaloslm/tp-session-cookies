f246b48d791d2617200081c6ebc06d4a01ec1156
DávalosLeonardo <davaloslm@hotmail.com.ar>
2021-10-05 20:08:33 -0300

    Crea index controller

--- sessionycookies/src/routes/index.js
+++ sessionycookies/src/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
+var { index } = require("../controllers/indexController")
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index);
 
 module.exports = router;
